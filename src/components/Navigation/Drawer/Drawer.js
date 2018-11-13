0a2a3d8a684774c903b4120da3b101b62e9918e0
Oleksandr <sankoniks@gmail.com>
2018-11-13 21:42:06 +0200

    React => adding fading for menu onClick

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import classes from './Drawer.css'
+import BackDrop from '../../UI/BackDrop/BackDrop'
 
 const links = [
     1, 2, 3
@@ -24,11 +25,14 @@ class Drawer extends Component {
         }
 
         return (
-            <nav className={cls.join(' ')}>
-                <ul>
-                    {this.renderLinks()}
-                </ul>
-            </nav>
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        {this.renderLinks()}
+                    </ul>
+                </nav>
+                { this.props.isOpen ?  <BackDrop onClick={this.props.onClose}/> : null }
+            </React.Fragment>
         )
     }
 }

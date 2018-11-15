4903b59ca5aa4bc08a29261cc6b8d5b59db0b53e
Oleksandr <sankoniks@gmail.com>
2018-11-15 23:18:34 +0200

    react-forms => modifyed Creat Test block

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,55 @@
 import React, {Component} from 'react'
+import classes from './QuizCreator.css'
+import Button from '../../components/UI/Button/Button'
 
 export default class QuizCreator extends Component {
-    render () {
+
+    submitHandler = event => {
+        event.preventDefault();
+    }
+
+    addAuestionHandler = () => {
+
+    };
+
+    createQuizHandler = () => {
+
+    };
+
+    render() {
         return (
-            <div>
-                <h1>QuizCreator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Creating test</h1>
+
+                    <form onSubmit={this.submitHandler}>
+
+                        <input type="text"/>
+                        <hr/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+
+
+                        <select></select>
+
+                        <Button
+                            type='primary'
+                            onClick={this.addAuestionHandler}
+                        >
+                            Add question
+                        </Button>
+
+                        <Button
+                            type='success'
+                            onClick={this.createQuizHandler}
+                        >
+                            Create test
+                        </Button>
+                    </form>
+
+                </div>
             </div>
         )
     }

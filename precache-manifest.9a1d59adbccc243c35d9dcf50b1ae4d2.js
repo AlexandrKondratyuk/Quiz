b3374687493a9f14e94a05b29e11a5957f9719ab
Oleksandr <sankoniks@gmail.com>
2018-11-16 01:36:26 +0200

    Updates

---
+++ precache-manifest.9a1d59adbccc243c35d9dcf50b1ae4d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f22d5387155722563924",
+    "url": "/Quiz/static/js/runtime~main.f22d5387.js"
+  },
+  {
+    "revision": "be509a8df6c64c844bab",
+    "url": "/Quiz/static/js/main.be509a8d.chunk.js"
+  },
+  {
+    "revision": "d1da963eace57dc99cdb",
+    "url": "/Quiz/static/js/1.d1da963e.chunk.js"
+  },
+  {
+    "revision": "be509a8df6c64c844bab",
+    "url": "/Quiz/static/css/main.f7434e5a.chunk.css"
+  },
+  {
+    "revision": "6a479ac75c08c596704b0eb267b05ddb",
+    "url": "/Quiz/index.html"
+  }
+];

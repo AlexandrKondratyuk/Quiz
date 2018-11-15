af4cb063e6c5aa8cb3d831efff39a6ff869565a6
Oleksandr <sankoniks@gmail.com>
2018-11-15 22:48:10 +0200

    react-forms => validation_part2

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import classes from './Auth.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
+import is from 'is_js'
 
 export default class Auth extends Component {
     state = {
@@ -44,8 +45,40 @@ export default class Auth extends Component {
         event.preventDefault();
     };
 
+    validateControl(value, validation) {
+        if (!validation){
+            return true;
+        }
+        let isValid = true;
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.email) {
+            isValid = is.email(value) && isValid;
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid;
+        }
+
+        return isValid;
+    }
+
     onChangeHandler = (event, controlName) => {
-        console.log(`${controlName}: `, event.target.value);
+        const formControls = { ...this.state.formControls };
+        const control = { ...formControls[controlName] };
+
+        control.value = event.target.value;
+        control.touched = true;
+        control.valid = this.validateControl(control.value, control.validation);
+
+        formControls[controlName] = control;
+
+        this.setState({
+            formControls
+        });
     };
 
     renderInput() {
@@ -53,7 +86,7 @@ export default class Auth extends Component {
             const control = this.state.formControls[controlName];
             return (
                 <Input
-                    key={controlName = index}
+                    key={controlName + index}
                     type={control.type}
                     value={control.value}
                     valid={control.valid}
